d843291dc6e5f97fe61e04f623b01aba94f7f8d1
Nanda Adisaputra <nandaadisaputra18@gmail.com>
2024-05-11 07:14:07 +0700

    Initial commit

---
+++ app/src/main/java/com/nandaadisaputra/tugasonemobileprogramming/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nandaadisaputra.tugasonemobileprogramming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
